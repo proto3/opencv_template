958a686c40fa0b7c0a244e9de90e353b2703b78e
Lucas Felix <lucas@sogilis.com>
2017-12-14 17:10:13 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,69 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <MyProject/MyClass.h>
 #include <iostream>
+#include <fstream>
+#include <sys/mman.h>
+#include <unistd.h>
+
+using namespace std;
 
 //----------------------------------------------------------------------------//
 int main(int argc, char **argv)
 {
-    //IMAGE INPUT------------------------//
-    cv::Mat img = cv::imread("../lena.jpg", cv::IMREAD_ANYDEPTH);
-    if(img.data == NULL)
+    size_t length = 512 * 512;
+    void* addr = mmap(NULL, sizeof(uint8_t)*length, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if(addr == MAP_FAILED)
     {
-        std::cerr << "Cannot read image." << std::endl;
-        return -1;
+        cout << "fail" << endl;
+        exit(-1);
     }
-    //process image here
-    cv::imshow("display", img);
-    cv::waitKey(0);
+    uint8_t* data = (uint8_t*)addr;
 
-    //VIDEO INPUT------------------------//
-    cv::Mat frame;
-    cv::VideoCapture video(0);
-    // cv::VideoCapture video("../video.mp4");
-    if(!video.isOpened())
+
+    //TODO FIND A WAY TO COPY image file to a buffer while checking its type and size 
+    // cv::Mat img(512, 512, CV_8UC1, data);
+    // img = imread("../lena.jpg", cv::IMREAD_GRAYSCALE);
+    // img = cv::Mat(512, 512, CV_8UC1, data);
+    std::ifstream file("../lena.jpg", std::ios::binary | std::ios::ate);
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+
+    std::vector<char> buffer(size);
+    if (file.read(buffer.data(), size))
     {
-        std::cerr << "Cannot open video source." << std::endl;
-        return -1;
-    }
-    while(video.read(frame))
-    {
-        //process frame here
-        cv::imshow("display", frame);
-        if((char)cv::waitKey(30) != -1)
-        {
-            break;
-        }
+        /* worked! */
     }
 
-    //CLASS TEMPLATE---------------------//
-    MyClass m;
-    m.foo();
+    cv::imdecode(buffer, IMREAD_GRAYSCALE, data);
+
+
+    int counter = 0;
+    pid_t pid = fork();
+
+    if(pid == 0)
+    {
+        // usleep(100000);
+        // int ret = munmap(addr, length);
+        cout << "child" << endl;
+        for(int i = 0; i < 10000 ; i++)
+            data[i] = i / 50;
+        cv::imshow("display", img);
+        cv::waitKey(0);
+        return 0;
+    }
+    else if(pid > 0)
+    {
+        usleep(100000);
+        data[5000] = 0;
+        cv::imshow("display", img);
+        cv::waitKey(0);
+        return 0;
+    }
+    else
+    {
+        cout << "fork failed" << endl;
+        return -1;
+    }
 
     return 0;
 }
